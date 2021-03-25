a4d313198f3a2cca48c13c34ce81bc7c201a9782
sdg97 <simone.delgatto@life365.eu>
2021-03-25 09:14:45 +0100

    attempt to deploy

--- IoT_Server/settings.py
+++ IoT_Server/settings.py
@@ -1,5 +1,5 @@
 import redis
 DEVICES_DESCRIPTIONS_DIR = '/descriptions'
-REDIS_ADDRESS='localhost'
-REDIS_PORT=6379
+REDIS_ADDRESS='redis/0'
+REDIS_PORT=4000
 REDIS_INSTANCE = redis.Redis(REDIS_ADDRESS, REDIS_PORT, db=0, decode_responses=True)
