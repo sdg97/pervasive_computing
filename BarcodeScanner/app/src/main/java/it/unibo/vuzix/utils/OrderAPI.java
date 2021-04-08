cf14aad40e1fdf1268b988e74c64de1f8b38058b
Sophia <sophia.fantoni@studio.unibo.it>
2021-04-08 10:55:41 +0200

    Add element in activities layout - completed + Insert path for server comunication

--- BarcodeScanner/app/src/main/java/it/unibo/vuzix/utils/OrderAPI.java
+++ BarcodeScanner/app/src/main/java/it/unibo/vuzix/utils/OrderAPI.java
@@ -4,13 +4,23 @@ public class OrderAPI {
     private static final String BASE_URL = "https://it2.life365.eu/";
     private static final String API = "api/";
     private static final String AUTH = "auth/";
-    private static final String LOGIN = "wharehouse?login=wh&password=wh365/";
+    private static final String LOGIN = "admin?login=wh&password=wh365/";
     private static final String ORDER = "order/";
 
 
     private static String getBaseResourceURL () {
-        return BASE_URL + API + AUTH
-                + LOGIN
-                + ORDER;
+        return BASE_URL + API;
     }
+
+    public static String getLoginURL(){
+        return getBaseResourceURL() +
+                AUTH +
+                LOGIN;
+    }
+
+    public static String getOrderURL(String idOrder, String jwt){
+        return getBaseResourceURL() +
+                ORDER + "/" + idOrder + "?jwt=" + jwt;
+    }
+
 }
