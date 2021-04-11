d58b5b7dc8bc5fc21b91fa23bf148fc8fa61173f
Sophia <sophia.fantoni@studio.unibo.it>
2021-04-11 18:07:10 +0200

    refactor ConnectActivity - set forklift fields

--- BarcodeScanner/app/src/main/java/it/unibo/vuzix/activities/ConnectActivity.java
+++ BarcodeScanner/app/src/main/java/it/unibo/vuzix/activities/ConnectActivity.java
@@ -15,17 +15,18 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import it.unibo.vuzix.controller.Controller;
 import it.unibo.vuzix.model.Forklift;
+import it.unibo.vuzix.utils.OrderAPI;
 import it.unibo.vuzix.utils.RaspberryAPI;
 
 public class ConnectActivity extends AppCompatActivity {
 
-    private static final int ACTIVITY_CONNECT_CODE = 1;
     public static final String FORKLIFT_KEY = "FORKLIFT";
-
+    private static final int ACTIVITY_CONNECT_CODE = 1;
     private Forklift forklift;
     private Button confirmButton;
     private Button backButton;
@@ -40,46 +41,14 @@ public class ConnectActivity extends AppCompatActivity {
 
         confirmButton = findViewById(R.id.button);
         confirmButton.setOnClickListener(v -> {
-            String boxBarcode = editText.getText().toString(); //1
-            System.out.println("Confirm to connect to box " + boxBarcode);
+            startUse();
+            setJwtForklift();
 
-            if(isValid(boxBarcode)){
-                this.forklift.setIdRaspberry(Integer.getInteger(boxBarcode));
-                this.forklift.setPlacementNumber(2);
+            //shared forklift with OrderActivity
+            Intent intent = new Intent(this, OrderActivity.class);
+            intent.putExtra(FORKLIFT_KEY, forklift);
+            startActivityForResult(intent, ACTIVITY_CONNECT_CODE);
 
-                //localhost:5000/smartForklift/1/action/startUse
-                String url = RaspberryAPI.setStartUse(boxBarcode);
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                        Request.Method.POST,
-                        url,
-                        null,
-                        new Response.Listener<JSONObject>() {
-                            //{placements: [0, 1]}
-                            //todo
-                            @Override
-                            public void onResponse(JSONObject response) {
-                                System.out.println("Risposta " + response.toString());
-                            }
-                        },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error)
-                            {
-                                //TODO ERROR
-                                Toast.makeText(ConnectActivity.this, "Connection error to the box", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                Controller.getInstance(this).addToRequestQueue(jsonObjectRequest);
-
-                //shared forklift with OrderActivity
-                Intent intent = new Intent(this, OrderActivity.class);
-                intent.putExtra(FORKLIFT_KEY, forklift);
-                startActivityForResult(intent, ACTIVITY_CONNECT_CODE);
-            } else {
-                Toast.makeText(ConnectActivity.this, "Get valid box code", Toast.LENGTH_SHORT).show();
-            }
-            //todo partire chiamata di richiesta di connessione al box
-            //todo popup o testo di conferma poi cambio schermata
             launchActivity(OrderActivity.class);
         });
         confirmButton.setEnabled(false);
@@ -96,14 +65,16 @@ public class ConnectActivity extends AppCompatActivity {
         editText.addTextChangedListener(new TextWatcher() {
 
             @Override
-            public void afterTextChanged(Editable s) {}
+            public void afterTextChanged(Editable s) {
+            }
 
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(s.length() != 0)
+                if (s.length() != 0)
                     confirmButton.setEnabled(true);
                 else
                     confirmButton.setEnabled(false);
@@ -119,4 +90,67 @@ public class ConnectActivity extends AppCompatActivity {
         startActivity(new Intent(this, clazz));
     }
 
+    private void setJwtForklift() {
+        //http://it2.life365.eu/api/auth/admin?login=wh&password=wh365
+        String url = OrderAPI.getLoginURL();
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                null,
+                response -> {
+                    System.out.println(response);
+                    try {
+                        //System.out.println(response.getString("jwt"));
+                        forklift.setJwt(response.getString("jwt"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                },
+                error -> {
+                    //TODO ERROR
+                    Toast.makeText(ConnectActivity.this, "Can't reach jwt", Toast.LENGTH_SHORT).show();
+                });
+        Controller.getInstance(this).addToRequestQueue(jsonObjectRequest);
+    }
+
+    private void startUse() {
+
+        String boxBarcode = editText.getText().toString(); //1
+        System.out.println("Confirm to connect to box " + boxBarcode);
+
+        if (isValid(boxBarcode)) {
+            this.forklift.setIdRaspberry(Integer.getInteger(boxBarcode));
+            //localhost:5000/smartForklift/1/action/startUse
+            String url = RaspberryAPI.setStartUse(boxBarcode);
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.POST,
+                    url,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        //{placements: [0, 1]}
+                        //todo
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            System.out.println("Risposta " + response.toString());
+                            try {
+                                //placements dovrebbe essere un array di Int
+                                forklift.setPlacementNumber(response.getJSONArray("placements").length());
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            //TODO ERROR
+                            Toast.makeText(ConnectActivity.this, "Connection error to the box", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+            Controller.getInstance(this).addToRequestQueue(jsonObjectRequest);
+        } else {
+            Toast.makeText(ConnectActivity.this, "Get valid box code", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
